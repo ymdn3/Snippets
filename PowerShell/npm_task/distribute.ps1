3d929958105c8e66c69ab486da96703260517bcb
ymdn3 <yamadon@gmail.com>
2017-01-21 23:53:20 +0900

    "npm_modules" から選択ファイルをコピーするタスク処理

---
+++ PowerShell/npm_task/distribute.ps1
@@ -0,0 +1,82 @@
+﻿param([string[]]$func_list)
+$path_cd = Split-Path -Parent $MyInvocation.MyCommand.Path
+. "$path_cd\functions.ps1"
+
+#ファイル配置レイアウト定義
+Distribute $func_list @(
+	@{
+		module = "bootstrap";
+		output = "..\..\applications\Sandbox.Web";
+		directory = @(
+			@{ where = "\dist\css";   recurse = $false; dest = "Content\bootstrap\css" }
+			@{ where = "\dist\fonts"; recurse = $false; dest = "Content\bootstrap\fonts" }
+			@{ where = "\dist\js";    recurse = $false; dest = "Scripts\bootstrap\" }
+		);
+	}
+	@{
+		module = "jquery";
+		output = "..\..\applications\Sandbox.Web";
+		directory = @(
+			@{ where = "\dist"; recurse = $false; dest = "Scripts\jquery" }
+		);
+	}
+	@{
+		module = "jquery-ui-1-11-4";
+		output = "..\..\applications\Sandbox.Web";
+		directory = @(
+			@{ where = "\images"; recurse = $false; dest = "Content\jquery-ui\images" }
+		);
+		file = @(
+			@{ where = ""; search = "*.css"; dest = "Content\jquery-ui" }
+			@{ where = "";  search = "*.js"; dest = "Scripts\jquery-ui" }
+		);
+	}
+	@{
+		module = "jquery-ui-1-11-1";
+		output = "..\..\applications\Sandbox.Web";
+		file = @(
+			@{ where = "\ui";                  search = "*.js"; dest = "Scripts\jquery-ui\widgets" }
+			@{ where = "\ui\i18n"; search = "datepicker-ja.js"; dest = "Scripts\jquery-ui\widgets" }
+		);
+	}
+	@{
+		module = "vue";
+		output = "..\..\applications\Sandbox.Web";
+		file = @(
+			@{ where = "\dist"; search = "*.js"; dest = "Scripts\vue" }
+		);
+	}
+	@{
+		module = "slickgrid";
+		output = "..\..\applications\Sandbox.Web";
+		directory = @(
+			@{ where = "\images"; recurse = $false; dest = "Content\slickgrid\images" }
+		);
+		file = @(
+			@{ where = "";               search = "*.css"; dest = "Content\slickgrid" }
+			@{ where = "";                search = "*.js"; dest = "Scripts\slickgrid"}
+			@{ where = "\controls";      search = "*.css"; dest = "Content\slickgrid\controls" }
+			@{ where = "\controls";       search = "*.js"; dest = "Scripts\slickgrid\controls"}
+			@{ where = "\plugins";       search = "*.css"; dest = "Content\slickgrid\plugins" }
+			@{ where = "\plugins";        search = "*.js"; dest = "Scripts\slickgrid\plugins"}
+			@{ where = "\lib";           search = "*.css"; dest = "Content\slickgrid\lib" }
+			@{ where = "\lib";            search = "*.js"; dest = "Scripts\slickgrid\lib"}
+			@{ where = "\example"; search = "example.css"; dest = "Content\slickgrid\example"}
+			@{ where = "\example";        search = "*.js"; dest = "Scripts\slickgrid\example"}
+		);
+	}
+	<# 定義サンプル
+	@{
+		output = "このプロジェクトフォルダをカレントとした、出力先相対パス";
+		module = "対象のモジュール名（node_modules以下のフォルダ名）";
+		directory = @(
+			配列。whereと一致するフォルダを、destへコピーします。recurseがtrueのときサブフォルダも対象とします。
+			@{ where = "\モジュールフォルダ配下のパス"; recurse = $true|$false; dest = "outputをカレントとしたコピー先相対パス" }
+		);
+		file = @(
+			配列。whereと一致するフォルダ配下の、searchと一致するファイルを、destへコピーします。
+			@{ where = "\モジュールフォルダ配下のパス"; search = "コピー対象。ワイルドカード指定可"; dest = "outputをカレントとしたコピー先相対パス" }
+		);
+	}
+	#>
+)
---
+++ PowerShell/npm_task/functions.ps1
@@ -0,0 +1,69 @@
+﻿function Distribute($private:func_list, $private:param_list) {
+	foreach ($f in $private:func_list) {
+		switch ($f) {
+			'clean' { 
+				$private:param_list | %{ clean $_ }
+			}
+			'dist' {
+				$private:param_list | %{ dist (path-module $_.module) $_ }
+			}
+		}
+	}
+}
+
+function clean($private:param) {
+	$path_output = (path-output $private:param.output)
+	@($private:param.directory, $private:param.file) | %{
+		$_ | ? { $_ -ne $null } | %{
+			($tgt = [IO.Path]::Combine($path_output, $_.dest)) |oh
+			if (Test-Path $tgt) {
+				Remove-Item $tgt -Recurse
+			}
+		}
+	}
+}
+function dist([string]$private:path, $private:param) {
+	dist-directory $private:path $private:param
+	dist-file $private:path $private:param
+	[IO.Directory]::EnumerateDirectories($private:path) | %{
+		dist $_ $private:param
+	}
+}
+function dist-directory([string]$private:path, $private:param) {
+	if ($private:param.directory -eq $null) { return }
+	$layout = $private:param.directory
+	$path_output = (path-output $private:param.output)
+	$path_module = (path-module $private:param.module)
+	$path_rel = $private:path.Remove(0, $path_module.Length)
+
+	$layout | ?{ $path_rel -eq $_.where } | %{
+		$dst = [IO.Path]::Combine($path_output, $_.dest)
+		($src = [IO.Path]::Combine($private:path, "*"))|oh
+		New-Item "$dst" -type directory -force |Out-Null
+		if ($_.recurse) {
+			Copy-Item $src "$dst" -Recurse
+		} else {
+			Copy-Item $src "$dst"
+		}
+	}
+}
+function dist-file([string] $private:path, $private:param) {
+	if ($private:param.file -eq $null) { return }
+	$layout = $private:param.file
+	$path_output = (path-output $private:param.output)
+	$path_module = (path-module $private:param.module)
+	$path_rel = $private:path.Remove(0, $path_module.Length)
+
+	$layout | ?{ $path_rel -eq $_.where } | %{
+		$dst = [IO.Path]::Combine($path_output, $_.dest)
+		($src = [IO.Path]::Combine($private:path, $_.search))|oh
+		New-Item "$dst" -type directory -force |Out-Null
+		Copy-Item $src "$dst"
+	}
+}
+function path-module([string] $private:name) {
+	[IO.Path]::Combine((pwd).Path, "node_modules", $private:name)
+}
+function path-output([string] $private:output) {
+	[IO.Path]::Combine((pwd).Path, $private:output)
+}
