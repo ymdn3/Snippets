81ee81365ac7666f10deb0e5e68ae33f96538175
ymdn3 <yamadon@gmail.com>
2017-02-24 19:37:05 +0900

    add Tasks

---
+++ projects/KOILib.Common/Core/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOILib.Common.Core
+{
+    public class Tasks
+    {
+        public static IEnumerable<Task> From(params Action[] actions)
+        {
+            return actions.Select(action => Task.Run(action));
+        }
+        public static IEnumerable<Task<T>> From<T>(params Func<T>[] functions)
+        {
+            return functions.Select(function => Task.Run(function));
+        }
+    }
+}
