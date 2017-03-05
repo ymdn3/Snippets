6fa1b62b29befece04d9e78a13016ba592d27ee2
ymdn3 <yamadon@gmail.com>
2017-03-05 23:06:05 +0900

    sync

---
+++ projects/KOILib.Common.Aspmvc/Models/ComplexValueItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOILib.Common.Aspmvc.Models
+{
+    public abstract class ComplexValueItem
+    {
+        public virtual string Text { get; set; }
+        public virtual string Value { get; set; }
+
+        public ComplexValueItem()
+        {
+            Text = "";
+            Value = "";
+        }
+    }
+}
---
+++ projects/KOILib.Common.Aspmvc/Models/RadioItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOILib.Common.Aspmvc.Models
+{
+    /// <summary>
+    /// HTMLラジオボタン項目モデル
+    /// </summary>
+    public class RadioItem
+    {
+        public static readonly RadioItem Empty = new RadioItem();
+
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public RadioItem()
+        {
+            Text = "";
+            Value = "";
+        }
+    }
+}
