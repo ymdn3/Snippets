1d8566062e7286488971a14c8cd87096e65db212
ymdn3 <yamadon@gmail.com>
2017-03-12 18:46:07 +0900

    add AcceptRouteAttribute class

---
+++ projects/KOILib.Common.Aspmvc/MethodSelectors/AcceptRouteAttribute.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using KOILib.Common.Extensions;
+
+namespace KOILib.Common.Aspmvc.MethodSelectors
+{
+    /// <summary>
+    /// ルートパラメータに特定の値が存在するかどうかで妥当性を判断するメソッドセレクター
+    /// </summary>
+    public class AcceptRouteAttribute : ActionMethodSelectorAttribute
+    {
+        /// <summary>
+        /// チェック対象のルートパラメータ名
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// 妥当と判断するルートパラメータの値。
+        /// </summary>
+        public IEnumerable<string> Values { get; private set; } 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="controllerContext"></param>
+        /// <param name="methodInfo"></param>
+        /// <returns></returns>
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            var requestValue = controllerContext.RouteData.GetRequiredString(Name);
+            return requestValue.EqualsAny(Values, StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// 指定したいずれかの値との一致セレクターを生成します
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <param name="ormore"></param>
+        public AcceptRouteAttribute(string name, string value, params string[] ormore)
+        {
+            this.Name = name;
+            this.Values = ormore.Prepend(value);
+        }
+
+        /// <summary>
+        /// 指定した値との一致セレクターを生成します
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public AcceptRouteAttribute(string name, string value)
+        {
+            this.Name = name;
+            this.Values = new string[] { value };
+        }
+    }
+}
