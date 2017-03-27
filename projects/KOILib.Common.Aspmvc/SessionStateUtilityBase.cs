63dabdbd75f95424375eb4200f320c81cbcabdd2
ymdn3 <yamadon@gmail.com>
2017-03-28 00:07:48 +0900

    add expires function

--- projects/KOILib.Common.Aspmvc/SessionStateUtilityBase.cs
+++ projects/KOILib.Common.Aspmvc/SessionStateUtilityBase.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.SessionState;
+using KOILib.Common.Extensions;
 
 namespace KOILib.Common.Aspmvc
 {
@@ -22,6 +23,7 @@ namespace KOILib.Common.Aspmvc
             if (atOnce)
                 _HttpSession.Remove(key);
 
+            _ExpireManager.ResetExpire(key);
             return (T)value;
         }
 
@@ -33,17 +35,20 @@ namespace KOILib.Common.Aspmvc
         public void Clear()
         {
             _HttpSession.Clear();
+            _ExpireManager.Clear();
         }
 
-        public void Remove(string key)
-        {
-            _HttpSession.Remove(key);
-        }
-
-        public void Set<T>(string key, T value)
+        public void Set<T>(string key, T value, bool noExpire)
         {
             _HttpSession.Remove(key);
             _HttpSession[key] = value;
+
+            if (!noExpire)
+                _ExpireManager.SetExpire(key);
+        }
+        public void Set<T>(string key, T value)
+        {
+            Set(key, value, noExpire: false);
         }
 
         public bool Exists(string key)
@@ -56,17 +61,60 @@ namespace KOILib.Common.Aspmvc
             return Get<T>(key, true);
         }
 
-        public SessionStateUtilityBase(HttpContext context)
+        public SessionStateUtilityBase(HttpContext context) : this(new HttpSessionStateWrapper(context.Session))
         {
-            _HttpSession = new HttpSessionStateWrapper(context.Session);
         }
-        public SessionStateUtilityBase(HttpSessionState session)
+        public SessionStateUtilityBase(HttpSessionState session) : this(new HttpSessionStateWrapper(session))
         {
-            _HttpSession = new HttpSessionStateWrapper(session);
         }
         public SessionStateUtilityBase(HttpSessionStateBase session)
         {
             _HttpSession = session;
+            _ExpireManager = new ExpireManager(_HttpSession, 60 * 1000);
+        }
+
+        internal ExpireManager _ExpireManager;
+        internal class ExpireManager : WatcherBase
+        {
+            private HttpSessionStateBase _session;
+            private ConcurrentDictionary<string, DateTime> _expireTime;
+
+            protected override void TimerElapsedBody()
+            {
+                Sweep(DateTime.UtcNow);
+            }
+            internal void Sweep(DateTime utcReferenceTime)
+            {
+                _expireTime
+                    .Where(x => x.Value < utcReferenceTime)
+                    .ParallelDo(x => Expire(x.Key));
+            }
+            internal void Expire(string key)
+            {
+                var t = DateTime.UtcNow;
+                if (_expireTime.TryRemove(key, out t))
+                    _session.Remove(key);
+            }
+            internal void SetExpire(string key)
+            {
+                var extend = _session.Timeout;// Timeout設定分の延長
+                _expireTime.AddOrUpdate(key, DateTime.UtcNow.AddMinutes(extend), (_, t) => t.AddMinutes(extend));
+            }
+            internal void ResetExpire(string key)
+            {
+                if (_expireTime.ContainsKey(key))
+                    SetExpire(key);
+            }
+            internal void Clear()
+            {
+                _expireTime.Clear();
+            }
+            public ExpireManager(HttpSessionStateBase session, int interval)
+            {
+                _session = session;
+                _expireTime = new ConcurrentDictionary<string, DateTime>();
+                this.Start(interval);
+            }
         }
     }
 }
