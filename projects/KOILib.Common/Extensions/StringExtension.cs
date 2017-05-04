8fdd870e026dc484d38a0b5a1863cc6a6ba045db
ymdn3 <yamadon@gmail.com>
2017-05-04 22:36:55 +0900

    sync

--- projects/KOILib.Common/Extensions/StringExtension.cs
+++ projects/KOILib.Common/Extensions/StringExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,28 @@ namespace KOILib.Common.Extensions
 {
     public static class StringExtension
     {
+        [DllImport("kernel32.dll")]
+        extern private static int LCMapStringW(int locale, uint dwMapFlags, [MarshalAs(UnmanagedType.LPWStr)]string lpSrcStr, int cchSrc, [MarshalAs(UnmanagedType.LPWStr)]string lpDestStr, int cchDest);
+        private enum dwMapFlags: uint
+        {
+            NORM_IGNORECASE = 0x00000001,           //大文字と小文字を区別しません。
+            NORM_IGNORENONSPACE = 0x00000002,       //送りなし文字を無視します。このフラグをセットすると、日本語アクセント文字も削除されます。
+            NORM_IGNORESYMBOLS = 0x00000004,        //記号を無視します。
+            LCMAP_LOWERCASE = 0x00000100,           //小文字を使います。
+            LCMAP_UPPERCASE = 0x00000200,           //大文字を使います。
+            LCMAP_SORTKEY = 0x00000400,             //正規化されたワイド文字並び替えキーを作成します。
+            LCMAP_BYTEREV = 0x00000800,             //Windows NT のみ : バイト順序を反転します。たとえば 0x3450 と 0x4822 を渡すと、結果は 0x5034 と 0x2248 になります。
+            SORT_STRINGSORT = 0x00001000,           //区切り記号を記号と同じものとして扱います。
+            NORM_IGNOREKANATYPE = 0x00010000,       //ひらがなとカタカナを区別しません。ひらがなとカタカナを同じと見なします。
+            NORM_IGNOREWIDTH = 0x00020000,          //シングルバイト文字と、ダブルバイトの同じ文字とを区別しません。
+            LCMAP_HIRAGANA = 0x00100000,            //ひらがなにします。
+            LCMAP_KATAKANA = 0x00200000,            //カタカナにします。
+            LCMAP_HALFWIDTH = 0x00400000,           //半角文字にします（適用される場合）。
+            LCMAP_FULLWIDTH = 0x00800000,           //全角文字にします（適用される場合）。
+            LCMAP_LINGUISTIC_CASING = 0x01000000,   //大文字と小文字の区別に、ファイルシステムの規則（既定値）ではなく、言語上の規則を使います。LCMAP_LOWERCASE、または LCMAP_UPPERCASE とのみ組み合わせて使えます。
+            LCMAP_SIMPLIFIED_CHINESE = 0x02000000,  //中国語の簡体字を繁体字にマップします。
+            LCMAP_TRADITIONAL_CHINESE = 0x04000000, //中国語の繁体字を簡体字にマップします。
+        }
 
         #region System.String
 
@@ -81,7 +104,7 @@ namespace KOILib.Common.Extensions
         }
 
         /// <summary>
-        /// 
+        /// start と end で指定した位置の範囲の文字列を返します。
         /// </summary>
         /// <param name="self"></param>
         /// <param name="start"></param>
@@ -91,11 +114,11 @@ namespace KOILib.Common.Extensions
         {
             if (start < 0) start = self.Length + start;
             if (end < 0) end = self.Length + end;
-            return self.Substring(start, end - start);
+            return self.Substring(start, Math.Min(self.Length, end - start));
         }
 
         /// <summary>
-        /// 
+        /// start で指定した位置以後の文字列を返します。
         /// </summary>
         /// <param name="self"></param>
         /// <param name="start"></param>
@@ -194,6 +217,40 @@ namespace KOILib.Common.Extensions
             var option = remainEmpty ? StringSplitOptions.None : StringSplitOptions.RemoveEmptyEntries;
             var values = value.Split(new char[] { '\r', '\n', ',' }, option);
             return values;
+        }
+
+        /// <summary>
+        /// 文字列変換
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="flags"></param>
+        /// <returns></returns>
+        private static string ConvertMap(this string self, dwMapFlags flags)
+        {
+            var ci = System.Globalization.CultureInfo.CurrentCulture;
+            var result = ' '.Repeat(self.Length);
+            LCMapStringW(ci.LCID, (uint)flags, self, self.Length, result, result.Length);
+            return result;
+        }
+
+        /// <summary>
+        /// 半角を全角に変換します
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToFullWidth(this string self)
+        {
+            return ConvertMap(self, dwMapFlags.LCMAP_FULLWIDTH);
+        }
+
+        /// <summary>
+        /// 全角を半角に変換します
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToHalfWidth(this string self)
+        {
+            return ConvertMap(self, dwMapFlags.LCMAP_HALFWIDTH | dwMapFlags.LCMAP_KATAKANA);
         }
         #endregion
 
